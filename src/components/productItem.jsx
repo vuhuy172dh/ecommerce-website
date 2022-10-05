fc3dd7ccd32ae8d6cb21ba7fda72b385e7b1849e
Hoàng Văn Phúc <20521760@gm.uit.edu.vn>
2022-10-05 23:39:29 +0700

    #3 Responsive for laptop, fix productItem conponent

--- src/components/productItem.jsx
+++ src/components/productItem.jsx
@@ -1,7 +1,7 @@
 
 function ProductItem({Img, Name, Price}) {
   return (
-    <div>
+    <div className="pb-5">
       <img className="w-full object-cover" src= {Img} alt={Name}/>
       <h4 className="mt-6 mb-2 text-h4">{Name}</h4>
       <h4 className="text-[18px]">{Price}</h4>
--- src/pages/productListing.jsx
+++ src/pages/productListing.jsx
@@ -5,6 +5,7 @@ import img1 from '../assets/images/ThreeVases.png'
 import img2 from '../assets/images/CeilingLamp.png'
 import img3 from '../assets/images/SingleVase.png'
 import img4 from '../assets/images/DarkChair.png'
+import clubImg from '../assets/images/features3.png'
 
 function ProductListingPage() {
 
@@ -48,25 +49,58 @@ function ProductListingPage() {
   ]
 
   return (
-    <div className='w-[100%]'>
-      <div className="h-52 bg-gray-100 pt-8 px-6">
-        <h2 className="text-center mb-8 text-h2">View all products</h2>
-        <div className="flex gap-3">
+    <div>
+      {/* this is Header component */}
+      <header className=" bg-gray-100 py-8 px-6 laptop:bg-white laptop:pt-20 laptop:pb-3 laptop:px-20">
+        <h2 className="text-center mb-10 text-h2 laptop:mb-8">View all products</h2>
+        <div className="flex gap-3 laptop:hidden">
           <Button Size="small" Color='white' IconRight>Sorting</Button>
           <Button Size="small" Color='white' IconRight>Filtering</Button>
         </div>
-      </div>
-      <div className='px-6 py-7'>
-        <div className="grid grid-cols-2 gap-4">
+
+        {/* this is Navigation component */}
+        <nav className="hidden laptop:flex justify-between items-center px-6">
+          <div className="flex items-center gap-3">
+            <Button Size="small" Color='white' IconRight>Category</Button>
+            <Button Size="small" Color='white' IconRight>Product type</Button>
+            <Button Size="small" Color='white' IconRight>Price</Button>
+            <Button Size="small" Color='white' IconRight>Brand</Button>
+          </div>
+          <div className="flex items-center">
+            <p className="mr-4 text-[14px] font-normal font-satoshi">Sorting by:</p>
+            <Button Size="small" Color='white' IconRight>Date added</Button>
+          </div>
+        </nav>
+      </header>
+
+      {/* this is list Product component */}
+      <div className="px-6 py-7 laptop:px-20 laptop:pb-10">
+        <div className="grid grid-cols-2 gap-4 laptop:grid-cols-4 laptop:gap-x-5 laptop:gap-y-7">
               {
                 productItems.map(item => (
                   <ProductItem key={item.id} Img={item.imgUrl} Name={item.name} Price={item.price}/>
                 ))
               }
         </div>
-        <div className="flex mt-8">
+        {/* Load more Button */}
+        <div className="flex mt-8 laptop:max-w-[180px] laptop:mx-auto">
           <Button Color='secondary'>View collection</Button>
         </div>
+      </div>
+
+      {/* this is Contact component */}
+      <div className="hidden laptop:flex">
+          <img className="w-[50%]" src={clubImg} alt="join club" />
+          <div className="w-[50%] object-cover p-[70px] flex flex-col justify-between">
+              <div>
+                <h3 className="text-h2">Join the club and get the benefits</h3>
+                <p className="text-h5 my-5 w-[80%]">Sign up for our newsletter and receive exclusive offers on new ranges, sale, pop up store and more</p>
+              </div>
+              <div className="flex w-[80%]">
+                <input className="flex-1 bg-light_grey pl-8 outline-none" type="text" placeholder="your@email.com"/>
+                <div className="w-[118px]"><Button Color="primary">Signup</Button></div>
+              </div>
+          </div>
       </div>
     </div>
   ) 
