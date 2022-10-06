8b20928504b778bce0e80068869a649f83c43e40
Lê Trung Nghĩa <90697464+KyoNo2k1@users.noreply.github.com>
2022-10-06 12:12:16 +0700

    #11 Setup enviroment env
    Setup enviroment env to secure key

--- src/services/firebase.config.js
+++ src/services/firebase.config.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCJ-4lpgxjBXxBxzM3gembLR7-awTo520Y',
-  authDomain: 'ecommerce-naver.firebaseapp.com',
-  projectId: 'ecommerce-naver',
-  storageBucket: 'ecommerce-naver.appspot.com',
-  messagingSenderId: '788274062159',
-  appId: '1:788274062159:web:3b100ff257fdd9868a1b3a',
-  measurementId: 'G-JRXYQF9EX2'
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 }
 
 const app = initializeApp(firebaseConfig)
