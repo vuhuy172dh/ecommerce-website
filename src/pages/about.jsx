118df9d7602c5f0f46e27b6d44e67f707f6f84ae
Quang Vu <ngqangvvu@gmail.com>
2022-10-07 23:24:40 +0700

    #4 - Update styles and buttons

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,6 +3,7 @@ import InfoItem from '../components/infoItem'
 import EmailSignUp from '../components/emailSignUp'
 import feature2 from '../assets/images/features1.png'
 import feature3 from '../assets/images/features3.png'
+import Button from '../components/button'
 
 function AboutPage() {
   return (
@@ -18,7 +19,7 @@ function AboutPage() {
       <div className="grid grid-cols-1 laptop:grid-cols-2 mx-auto">
         {/* Feature 1 */}
         <div>
-          <div className="px-6 mt-12 sm:mb-9 laptop:px-28  laptop:pt-20 ">
+          <div className="px-6 mt-12 mb-9 laptop:px-28  laptop:pt-20 ">
             <div className="text-h4 laptop:text-6">
               From a studio in London to a global brand with over 400 outlets
             </div>
@@ -30,40 +31,50 @@ function AboutPage() {
               live, breathe and design so our Chelsea boutique become the hotbed
               for the London interior design community.
             </p>
-            <button className="laptop:w-2/5 laptop:mt-40 mobile:mb-16 mt-16 w-full h-14 bg-light_grey">
-              Get in touch
-            </button>
+            <div className="flex w-full laptop:w-2/5 laptop:mt-48 mobile:mb-16 mt-16">
+              <Button
+                className="laptop:mt-40 mobile:mb-16 mt-16"
+                Size="medium"
+                Color="secondary"
+                State="default"
+              >
+                Get in touch
+              </Button>
+            </div>
           </div>
         </div>
         {/* Feature 2 */}
         <div>
-          <img
-            src={feature2}
-            alt="feature2"
-            className=" w-full  object-cover"
-          />
+          <img src={feature2} alt="feature2" className="object-cover" />
         </div>
         {/* Feature 3 */}
         <div>
-          <img src={feature3} alt="feature1" className="  h-full object-cover" />
+          <img src={feature3} alt="feature1" className="object-cover" />
         </div>
         {/* Feature 4*/}
         <div>
-          <div className="px-6 mt-12 mb-9 laptop:px-28  laptop:pt-20" >
+          <div className="px-6 mt-12 mb-9 laptop:px-28  laptop:pt-20">
             <div className="text-h4 laptop:text-6 ">
               Our service isn’t just personal, it’s actually hyper personally
               exquisite
             </div>
-            <p className="font-satoshi text-3.5 mt-3 laptop:text-4">
+            <p className="text-body-sm laptop:text-body-md mt-3 laptop:text-4">
               When we started Avion, the idea was simple. Make high quality
               furniture affordable and available for the mass market. <br />
               <br /> Handmade, and lovingly crafted furniture and homeware is
               what we live, breathe and design so our Chelsea boutique become
               the hotbed for the London interior design community.
             </p>
-            <button className="laptop:w-2/5 laptop:mt-48 mt-16 mb-16 w-full h-14 bg-light_grey">
-              Get in touch
-            </button>
+            <div className="flex w-full laptop:w-2/5 laptop:mt-48 mobile:mb-16 mt-16">
+              <Button
+                className="laptop:mt-40 mobile:mb-16 mt-16"
+                Size="medium"
+                Color="secondary"
+                State="default"
+              >
+                Get in touch
+              </Button>
+            </div>
           </div>
         </div>
       </div>
