0f95cf09c58da37bb973136f0d3cce0d927ad208
HUY VU <quochuy123dh@gmail.com>
2022-10-08 09:23:49 +0700

    #4 - fixed some UI

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -23,7 +23,7 @@ function AboutPage() {
             <div className="text-h4 laptop:text-6">
               From a studio in London to a global brand with over 400 outlets
             </div>
-            <p className="font-satoshi text-3.5 mt-3 laptop:text-4">
+            <p className="text-body-sm mt-3 laptop:text-body-md">
               When we started Avion, the idea was simple. Make high quality
               furniture affordable and available for the mass market. <br />{' '}
               <br />
@@ -45,11 +45,19 @@ function AboutPage() {
         </div>
         {/* Feature 2 */}
         <div>
-          <img src={feature2} alt="feature2" className="object-cover" />
+          <img
+            src={feature2}
+            alt="feature2"
+            className="object-cover w-full h-full"
+          />
         </div>
         {/* Feature 3 */}
         <div>
-          <img src={feature3} alt="feature1" className="object-cover" />
+          <img
+            src={feature3}
+            alt="feature1"
+            className="object-cover w-full h-full"
+          />
         </div>
         {/* Feature 4*/}
         <div>
@@ -58,7 +66,7 @@ function AboutPage() {
               Our service isn’t just personal, it’s actually hyper personally
               exquisite
             </div>
-            <p className="text-body-sm laptop:text-body-md mt-3 laptop:text-4">
+            <p className="text-body-sm laptop:text-body-md mt-3">
               When we started Avion, the idea was simple. Make high quality
               furniture affordable and available for the mass market. <br />
               <br /> Handmade, and lovingly crafted furniture and homeware is
@@ -66,12 +74,7 @@ function AboutPage() {
               the hotbed for the London interior design community.
             </p>
             <div className="flex w-full laptop:w-2/5 laptop:mt-48 mobile:mb-16 mt-16">
-              <Button
-                className="laptop:mt-40 mobile:mb-16 mt-16"
-                Size="medium"
-                Color="secondary"
-                State="default"
-              >
+              <Button Size="medium" Color="secondary" State="default">
                 Get in touch
               </Button>
             </div>
