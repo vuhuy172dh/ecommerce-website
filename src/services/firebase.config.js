db1a231aec13b2ad1f353fa127b9ef375d36fff8
tuannt02 <20522122@gm.uit.edu.vn>
2022-10-09 16:25:49 +0700

    #18 - reconfig

--- src/services/firebase.config.js
+++ src/services/firebase.config.js
@@ -8,9 +8,13 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 }
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig)
 
-export const db = getFirestore(app)
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+export default app;
+export { db }
