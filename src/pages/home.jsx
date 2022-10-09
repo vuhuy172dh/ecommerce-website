66a08632ea56e27fe96adaf6638ca9a61cb66616
HUY VU <quochuy123dh@gmail.com>
2022-10-09 12:51:16 +0700

    upgrade productItem

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -65,37 +65,37 @@ function HomePage() {
   return (
     <div className="w-full flex flex-col">
       {/* homepage poster */}
-      <div className="w-full flex flex-col mb-8 relative">
-        {/* poster content */}
-        <div className="w-full px-8 mb-8 flex flex-col justify-between top-1/2 tablet:-translate-y-1/2 right-[5%] tablet:p-10 tablet:absolute tablet:w-1/2 laptop:w-2/5 tablet:h-1/2 tablet:bg-white">
-          <div className="w-full flex flex-col gap-2">
-            <p className="text-h3 tablet:text-h3 text-dark_primary">
-              Luxury homeware for people who love timeless design quality
-            </p>
-            <p className="text-body-md text-dark_primary tablet:hidden">
-              With our new collection, view over 400 bespoke pieces from
-              homeware through to furniture tody
-            </p>
-            <p className="tablet:block text-body-lg text-dark_primary">
-              Shop the new Spring 2022 collection today
-            </p>
-          </div>
-          <div className="flex w-full tablet:w-1/3">
-            <Link to="/products">
+      <div className="w-full flex justify-center">
+        <div className="w-[calc(100%-2rem)] tablet:w-[calc(100%-4rem)] flex flex-col mb-8 relative rounded-3xl shadow-xl shadow-gray-700/50 overflow-hidden hover:scale-[1.01] transition-all duration-500">
+          {/* poster content */}
+          <div className="w-full px-8 mb-8 flex flex-col justify-between top-1/2 tablet:-translate-y-1/2 right-[5%] tablet:p-10 tablet:absolute tablet:w-1/2 laptop:w-2/5 tablet:h-1/2 tablet:bg-white">
+            <div className="w-full flex flex-col gap-2">
+              <p className="text-h3 tablet:text-h3 text-dark_primary">
+                Luxury homeware for people who love timeless design quality
+              </p>
+              <p className="text-body-md text-dark_primary tablet:hidden">
+                With our new collection, view over 400 bespoke pieces from
+                homeware through to furniture tody
+              </p>
+              <p className="tablet:block text-body-lg text-dark_primary">
+                Shop the new Spring 2022 collection today
+              </p>
+            </div>
+            <div className="flex w-full tablet:w-1/3">
               <Button Size="medium" Color="secondary" State="default">
                 View collection
               </Button>
-            </Link>
+            </div>
           </div>
-        </div>
 
-        {/* poster image */}
-        <div className="w-full">
-          <img
-            src={homepage}
-            alt="poster"
-            className="w-full max-h-[700px] object-cover"
-          />
+          {/* poster image */}
+          <div className="w-full">
+            <img
+              src={homepage}
+              alt="poster"
+              className="w-full max-h-[700px] object-cover"
+            />
+          </div>
         </div>
       </div>
 
@@ -108,7 +108,7 @@ function HomePage() {
       <div className="px-8 my-8 tablet:my-20">
         {/* title */}
         <div className="my-4 w-fit">
-          <LinkButton path="/products">
+          <LinkButton path="/products" color="dark">
             <p className="text-h3">All Product</p>
           </LinkButton>
         </div>
