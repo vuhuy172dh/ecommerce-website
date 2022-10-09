66fe091147c8ae3098705e99808324fe662138c2
HUY VU <quochuy123dh@gmail.com>
2022-10-09 16:37:15 +0700

    update button component

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -20,28 +20,24 @@ const sizes = {
 
 const colors = {
   white: {
-    default:
-      'text-dark_primary bg-white hover:bg-light_grey focus:bg-white focus:border-2 focus:border-solid focus:border-primary',
+    default: 'text-dark_primary bg-white hover:bg-light_grey',
     disabled: 'text-dark_primary bg-white opacity-50'
   },
   ghost: {
-    default:
-      'bg-transparent text-dark_primary hover:bg-border_grey focus:bg-transparent focus:border-2 focus:border-solid focus:border-primary',
+    default: 'bg-transparent text-dark_primary hover:bg-border_grey',
     disabled: 'bg-transparent text-dark_primary opacity-50'
   },
   secondary: {
-    default:
-      'bg-light_grey text-dark_primary hover:bg-border_grey focus:bg-light_grey focus:border-2 focus:border-solid focus:border-primary',
+    default: 'bg-light_grey text-dark_primary hover:bg-border_grey',
     disabled: 'bg-light_grey text-dark_primary opacity-50'
   },
   primary: {
-    default:
-      'bg-dark_primary text-white hover:bg-[#1e193e] focus:bg-dark_primary focus:border focus:border-solid focus:border-primary',
+    default: 'bg-dark_primary text-white hover:bg-[#1e193e]',
     disabled: 'bg-dark_primary text-white opacity-50'
   },
   opaque: {
     default:
-      'bg-[rgba(249,249,249,0.15)] hover:bg-[rgba(249,249,249,0.3)] focus:border-2 focus:border-solid focus:border-primary text-white',
+      'bg-[rgba(249,249,249,0.15)] hover:bg-[rgba(249,249,249,0.3)] text-white',
     disabled: 'bg-[rgba(249,249,249,0.15)] text-white opacity-50'
   }
 }
@@ -57,8 +53,8 @@ function Button({
   Padding = ''
 }) {
   return (
-    <motion.div
-      className={`text-body-md min-w-fit min-h-fit flex-1 flex items-center justify-center gap-4 relative ease-out duration-300 ${
+    <motion.button
+      className={`text-body-md min-w-fit min-h-fit flex-1 flex items-center justify-center gap-4 relative ease-out duration-300 cursor-pointer rounded-lg ${
         colors[Color][State]
       } ${Custom ? Padding : sizes[Size]}`}
       whileHover={{ scale: 1.05 }}
@@ -67,7 +63,7 @@ function Button({
     >
       {children}
       {IconRight && <FontAwesomeIcon icon={faCaretDown} />}
-    </motion.div>
+    </motion.button>
   )
 }
 
