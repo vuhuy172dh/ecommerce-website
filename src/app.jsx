f75f864c8adb1eeef65c8e3536a9d871647e85e5
HUY VU <quochuy123dh@gmail.com>
2022-10-12 00:24:28 +0700

    remove firebase test in app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -14,26 +14,6 @@ import SignIn from './pages/signIn'
 import SignUp from './pages/signUp'
 import ForgetPassword from './pages/forgetPassword'
 
-import { db } from './services/firebase.config'
-
-import { collection, addDoc } from "firebase/firestore"; 
-
-async function test() {
-  try {
-    const docRef = await addDoc(collection(db, "users"), {
-      first: "Ada",
-      last: "Lovelace",
-      born: 1815
-    });
-    console.log("Document written with ID: ", docRef.id);
-  } catch (e) {
-    console.error("Error adding document: ", e);
-  }
-  
-}
-
-test()
-
 function App() {
   return (
     <div>
