6aa97fa967534fc99b94dcca5dec8a16376cdfac
HUY VU <quochuy123dh@gmail.com>
2022-10-12 22:53:32 +0700

    #20 - add sidebar for user account page

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Router } from 'react-router-dom'
+import { Routes, Route, Outlet } from 'react-router-dom'
 import AboutPage from './pages/about'
 import HomePage from './pages/home'
 import ProductCartPage from './pages/productCart'
@@ -9,10 +9,21 @@ import Navbar from './components/navbar'
 import Footer from './components/footer'
 import NavDrawer from './components/navDrawer'
 import NavMode from './hooks/useNavMode'
-import User from './pages/user'
 import SignIn from './pages/signIn'
 import SignUp from './pages/signUp'
 import ForgetPassword from './pages/forgetPassword'
+import Sidebar from './components/user/sidebar'
+import Address from './components/user/address'
+import Profile from './components/user/profile'
+import Purchase from './components/user/purchase'
+import Wishlist from './components/user/wishlist'
+
+const SidebarLayout = () => (
+  <div className="laptop:flex">
+    <Sidebar />
+    <Outlet />
+  </div>
+)
 
 function App() {
   return (
@@ -27,7 +38,12 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
-          <Route path="/user/account/profile" element={<User />} />
+          <Route element={<SidebarLayout />}>
+            <Route path="/user/account/profile" element={<Profile />} />
+            <Route path="/user/account/address" element={<Address />} />
+            <Route path="/user/account/purchases" element={<Purchase />} />
+            <Route path="/user/account/wishlist" element={<Wishlist />} />
+          </Route>
           <Route path="/producCart" element={<ProductCartPage />} />
           <Route path="/products" element={<ProductListingPage />} />
           <Route path="/product/:productId" element={<ProductDetailPage />} />
