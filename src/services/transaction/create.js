4988f92d6ca7b6c7ddec6c5837db4ac967350102
HUY VU <quochuy123dh@gmail.com>
2022-10-22 09:06:43 +0700

    #46 - fixed some variable for create transaction service

--- src/services/transaction/create.js
+++ src/services/transaction/create.js
@@ -3,11 +3,12 @@ import { db } from '../firebase.config'
 import { TRANSACTIONS } from '../constant/firestore'
 
 const createOneTransaction = async (
-  uuidUser,
+  uidUser,
   contact,
   shipTo,
   shippingMethod,
   payment,
+  totalPrice,
   products
 ) => {
   try {
@@ -16,19 +17,21 @@ const createOneTransaction = async (
 
     // Khởi tạo data cho transaction
     const data = {
-      user: uuidUser,
-      contact,
+      user: uidUser,
+      contact: contact,
       ship_to: shipTo,
       shipping_method: shippingMethod,
-      payment,
+      payment: payment,
       status: 'In progress', // Lần đầu tạo bill sẽ ở trạng thái này
-      products,
+      products: products,
+      total_price: totalPrice,
       created_date: serverTimestamp()
     }
 
     // Sau khi thêm thành công một doc, trả về một docRef
-    await addDoc(transactionCollectionRef, data)
-
+    const docRef = await addDoc(transactionCollectionRef, data)
+    data.uid = docRef.id
+    return Promise.resolve(data)
     // Do sth here
   } catch (e) {
     const { code } = e
