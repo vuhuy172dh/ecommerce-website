64c88272850359ba0bfd8f1bcd3a6d83137b0e44
HUY VU <quochuy123dh@gmail.com>
2022-10-24 16:05:53 +0700

    update mobile responsive for product cart

--- src/components/productItem.jsx
+++ src/components/productItem.jsx
@@ -34,7 +34,7 @@ function ProductItem({ product }) {
   }
 
   return (
-    <div className="m-w-[200px] flex justify-center w-full rounded-lg overflow-hidden relative group shadow-lg shadow-gray-700/40 cursor-pointer">
+    <div className="m-w-[200px] flex flex-col laptop:flex-row justify-center w-full rounded-lg overflow-hidden relative group shadow-lg shadow-gray-700/40 cursor-pointer">
       <img
         src={product.arrImg?.[0]}
         alt="Product"
@@ -43,7 +43,7 @@ function ProductItem({ product }) {
       />
 
       {/* this is information */}
-      <div className="w-4/5 absolute bottom-4 z-20 px-4 flex flex-col items-center bg-light_grey dark:bg-secondary rounded-xl translate-y-[calc(100%+16px)] group-hover:translate-y-[calc(70%+16px)] hover:!translate-y-0 transition-all duration-200 dark:text-light_grey">
+      <div className="laptop:flex hidden w-4/5 absolute bottom-4 z-20 px-4 flex-col items-center bg-light_grey dark:bg-secondary rounded-xl translate-y-[calc(100%+16px)] group-hover:translate-y-[calc(70%+16px)] hover:!translate-y-0 transition-all duration-200 dark:text-light_grey">
         <p className="text-center text-h4 py-2">
           <strong>INFO</strong>
         </p>
@@ -63,6 +63,32 @@ function ProductItem({ product }) {
             onClick={() => handleAddToCart(1, product, userUid)}
           >
             Add to cart
+          </Button>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center laptop:hidden">
+        <p className="text-center text-h5 py-1">
+          <strong>INFO</strong>
+        </p>
+        <hr className="w-[90%] border-t border-t-dark_primary dark:border-t-light_grey" />
+        <div className="w-full py-2 px-2">
+          <p className="line-clamp-2 text-h6">
+            <strong>Name:</strong> {product.name}
+          </p>
+          <p className="line-clamp-2 text-h6">
+            <strong>Price:</strong> {product.price}$
+          </p>
+        </div>
+        <div className="px-2 mb-4">
+          <Button
+            Size="small"
+            Color="primary"
+            Custom={true}
+            Padding="px-4 py-2"
+            onClick={() => handleAddToCart(1, product, userUid)}
+          >
+            <p className="text-h6">Add to cart</p>
           </Button>
         </div>
       </div>
